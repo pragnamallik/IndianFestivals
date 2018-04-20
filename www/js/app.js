524205c44176a24757780f308b0552756e5b7c6a
pragnamallik <pragna.bangalore@gmail.com>
2018-04-20 17:36:13 -0400

    I1

--- www/js/app.js
+++ www/js/app.js
@@ -133,12 +133,12 @@ angular.module('myapp', ['ionic', 'firebase'])
       states[Connection.CELL_3G] = 'Cell 3G connection';
       states[Connection.CELL_4G] = 'Cell 4G connection';
       states[Connection.NONE] = 'No network connection';
-      if(states[network]==states[Connection.NONE])
-      {
+      alert(states[network]);
+      if (states[network] == states[Connection.NONE]) {
         document.getElementById("diwaliyoutube").innerHTML = "To view this video, you need to have active Internet";
       }
     }
-    
+
     $scope.gotoHomePage = function () {
       //$scope.checkConnection();
       window.location = "homepage.html";
